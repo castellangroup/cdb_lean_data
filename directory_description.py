b644e2a1847cc0f1871327ed630c5d29312d4254
ea083 <evanathana@gmail.com>
2024-02-24 17:40:11 -0500

    Added folder file list for factor data

--- directory_description.py
+++ directory_description.py
@@ -8,7 +8,7 @@ directory = 'equity'
 folders = [folder for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory, folder))]
 
 # Path to the output CSV file
-output_file = 'folder_file_list.csv'
+output_file = 'equity_folder_file_list.csv'
 
 # Open the CSV file in write mode
 with open(output_file, 'w', newline='') as csvfile:
@@ -25,7 +25,30 @@ with open(output_file, 'w', newline='') as csvfile:
         for file in files:
             writer.writerow([folder, file])
 
+print(f"CSV file '{output_file}' created successfully.")
 
-    
+# Path to the 'equity' directory
+directory = 'factor\eps'
+
+# Get a list of all folders inside the 'equity' directory
+folders = [folder for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory, folder))]
+
+# Path to the output CSV file
+output_file = 'factor_folder_file_list.csv'
+
+# Open the CSV file in write mode
+with open(output_file, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+
+    # Write the header row
+    writer.writerow(['Folder Name', 'File Name'])
+
+    # Write each folder name as a row in the CSV file
+    for folder in folders:
+        #writer.writerow([folder])
+        # for each folder, write the files in the folder in the next column
+        files = os.listdir(os.path.join(directory, folder))
+        for file in files:
+            writer.writerow([folder, file])
 
 print(f"CSV file '{output_file}' created successfully.")
