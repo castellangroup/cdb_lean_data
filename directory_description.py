2aff9fcf6acabddac6ec3be55da8238b09b58299
ea083 <evanathana@gmail.com>
2024-02-24 16:17:46 -0500

    Added script to make csv file describing folder structure

---
+++ directory_description.py
@@ -0,0 +1,31 @@
+import os
+import csv
+
+# Path to the 'equity' directory
+directory = 'equity'
+
+# Get a list of all folders inside the 'equity' directory
+folders = [folder for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory, folder))]
+
+# Path to the output CSV file
+output_file = 'folder_file_list.csv'
+
+# Open the CSV file in write mode
+with open(output_file, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+
+    # Write the header row
+    writer.writerow(['Folder Name', 'File Name'])
+
+    # Write each folder name as a row in the CSV file
+    for folder in folders:
+        #writer.writerow([folder])
+        # for each folder, write the files in the folder in the next column
+        files = os.listdir(os.path.join(directory, folder))
+        for file in files:
+            writer.writerow([folder, file])
+
+
+    
+
+print(f"CSV file '{output_file}' created successfully.")
